c129a90174200c09a8a9538bd7bbd457b2a797f2
baiye959 <3383522774@qq.com>
2024-04-17 18:50:58 +0800

    init

---
+++ src/main/java/com/baiye959/usercenter/mapper/UserMapper.java
@@ -0,0 +1,8 @@
+package com.baiye959.usercenter.mapper;
+
+import com.baiye959.usercenter.model.User;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+public interface UserMapper extends BaseMapper<User> {
+
+}
